5db974a25ffc14d67b93bd76280e8b2e83a3d478
bikalp <bikalpcdr42@gmail.com>
2024-06-03 18:15:03 +0545

    Implementation is done for Api documentation(Swagger).

--- blog-app-apis/src/main/java/com/bikalp/blog/config/SecurityConfig.java
+++ blog-app-apis/src/main/java/com/bikalp/blog/config/SecurityConfig.java
@@ -18,10 +18,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @Configuration
 @EnableWebSecurity
+@EnableWebMvc
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
@@ -43,6 +45,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(request -> request
                         // Allow access to the login endpoint without authentication
                         .requestMatchers("/api/v1/auth/**").permitAll()
+//                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/swagger-resources/**", "/webjars/**").permitAll()
                         .requestMatchers(HttpMethod.GET).permitAll()
                         // Any other request requires authentication
                         .anyRequest().authenticated())
